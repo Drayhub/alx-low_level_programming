ea93cb9d7bd1ed5522376969f68329d4120e7a2b
Drayhub <olawayedamilare17@gmail.com>
2022-09-13 08:15:44 -0700

    Multiplication table 0-9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
  * times_table - Prints the 9 times table
@@ -23,7 +23,7 @@ void times_table(void)
 				_putchar(op + 48);
 			}
 			else
-i			{
+			{
 				_putchar((op / 10) + 48);
 				_putchar((op % 10) + 48);
 			}
